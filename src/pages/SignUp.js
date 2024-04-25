941c6ef1e2064c0b8b33b80ccd1566cc344ec6e7
Ryan <rlemus93@gmail.com>
2024-04-25 13:37:25 -0700

    fixed sign up form

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -34,110 +34,132 @@ const SignUp = ({ signUp }) => {
       <div className="sign-up-form">
         <h2 className="">Create an Account</h2>
         <Form onSubmit={handleSubmit(onSubmit)}>
-          <FormGroup floating>
-            <input
-              style={{
-                width: "30vw",
-              }}
-              id="firstName"
-              name="firstName"
-              placeholder="First Name"
-              type="text"
-              className="form-control"
-              {...register("firstName", { required: true })}
-            />
-            <Label for="firstName">Enter Your First Name</Label>
-            {errors.email && (
-              <span className="form-validations">First name is required</span>
-            )}
-          </FormGroup>
-          <FormGroup floating>
-            <input
-              style={{
-                width: "30vw",
-              }}
-              id="lastName"
-              name="lastName"
-              placeholder="Last Name"
-              type="text"
-              className="form-control"
-              {...register("lastName", { required: true })}
-            />
-            <Label for="lastName">Enter Your Last Name</Label>
-            {errors.email && (
-              <span className="form-validations">Last name is required</span>
-            )}
-          </FormGroup>
-          <FormGroup floating>
-            <input
-              style={{
-                width: "30vw",
-              }}
-              id="email"
-              name="email"
-              placeholder="email"
-              type="email"
-              className="form-control"
-              {...register("email", { required: true })}
-            />
-            <Label for="email">Enter Your Email</Label>
-            {errors.email && (
-              <span className="form-validations">Email is required</span>
-            )}
-          </FormGroup>
-          <FormGroup floating>
-            <input
-              style={{
-                width: "30vw",
-              }}
-              id="username"
-              name="username"
-              placeholder="username"
-              type="username"
-              className="form-control"
-              {...register("username", { required: true })}
-            />
-            <Label for="email">Enter Your Username</Label>
-            {errors.email && (
-              <span className="form-validations">Username is required</span>
-            )}
-          </FormGroup>
-          <FormGroup floating>
-            <input
-              style={{
-                width: "30vw",
-              }}
-              id="password"
-              name="password"
-              placeholder="password"
-              type="password"
-              className="form-control"
-              {...register("password", { required: true })}
-            />
-            <Label for="password">Enter Your Password</Label>
-            {errors.password && (
-              <span className="form-validations">Password is required</span>
-            )}
-          </FormGroup>
-          <FormGroup floating>
-            <input
-              style={{
-                width: "30vw",
-              }}
-              id="password_confirmation"
-              name="password_confirmation"
-              placeholder="Password Confirmation"
-              type="Password"
-              className="form-control"
-              {...register("password_confirmation", { required: true })}
-            />
-            <Label for="password_confirmation">Password Confirmation</Label>
-            {errors.password_confirmation && (
-              <span className="form-validations">
-                Password Confirmation is required
-              </span>
-            )}
-          </FormGroup>
+          <Row>
+            <Col md={6}>
+              <FormGroup floating>
+                <input
+                  style={{
+                    width: "15vw",
+                  }}
+                  id="firstName"
+                  name="firstName"
+                  placeholder="First Name"
+                  type="text"
+                  className="form-control"
+                  {...register("firstName", { required: true })}
+                />
+                <Label for="firstName">Enter Your First Name</Label>
+                {errors.email && (
+                  <span className="form-validations">
+                    First name is required
+                  </span>
+                )}
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup floating>
+                <input
+                  style={{
+                    width: "15vw",
+                  }}
+                  id="lastName"
+                  name="lastName"
+                  placeholder="Last Name"
+                  type="text"
+                  className="form-control"
+                  {...register("lastName", { required: true })}
+                />
+                <Label for="lastName">Enter Your Last Name</Label>
+                {errors.email && (
+                  <span className="form-validations">
+                    Last name is required
+                  </span>
+                )}
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <FormGroup floating>
+                <input
+                  style={{
+                    width: "15vw",
+                  }}
+                  id="email"
+                  name="email"
+                  placeholder="email"
+                  type="email"
+                  className="form-control"
+                  {...register("email", { required: true })}
+                />
+                <Label for="email">Enter Your Email</Label>
+                {errors.email && (
+                  <span className="form-validations">Email is required</span>
+                )}
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup floating>
+                <input
+                  style={{
+                    width: "15vw",
+                  }}
+                  id="username"
+                  name="username"
+                  placeholder="username"
+                  type="username"
+                  className="form-control"
+                  {...register("username", { required: true })}
+                />
+                <Label for="email">Enter Your Username</Label>
+                {errors.email && (
+                  <span className="form-validations">Username is required</span>
+                )}
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <FormGroup floating>
+                <input
+                  style={{
+                    width: "15vw",
+                  }}
+                  id="password"
+                  name="password"
+                  placeholder="password"
+                  type="password"
+                  className="form-control"
+                  {...register("password", { required: true })}
+                />
+                <Label for="password">Enter Your Password</Label>
+                {errors.password && (
+                  <span className="form-validations">Password is required</span>
+                )}
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup floating>
+                <input
+                  style={{
+                    width: "15vw",
+                  }}
+                  id="password_confirmation"
+                  name="password_confirmation"
+                  placeholder="Password Confirmation"
+                  type="Password"
+                  className="form-control"
+                  {...register("password_confirmation", { required: true })}
+                />
+                <Label for="password_confirmation">Password Confirmation</Label>
+                {errors.password_confirmation && (
+                  <span className="form-validations">
+                    Password Confirmation is required
+                  </span>
+                )}
+              </FormGroup>
+            </Col>
+          </Row>
           <FormGroup>
             <Label for="profile_photo">Profile Photo</Label>
             <input
